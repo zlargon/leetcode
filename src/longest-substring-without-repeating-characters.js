20f9db4f9acbf618760cfea0bd7577a81c03dd88
zlargon <zlargon@icloud.com>
2015-12-01 01:00:10 +0800

    solve longest-substring-without-repeating-characters problem

--- src/longest-substring-without-repeating-characters.js
+++ src/longest-substring-without-repeating-characters.js
@@ -14,7 +14,33 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
+  var map = {}; // record last index of each character
+                // key = {character}, value = {last index}
 
+  var norepeat = 0; // norepeat index
+  var max = 0;
+
+  for (var index = 0; index < s.length; index++) {
+    var character = s.charAt(index);  // character
+
+    // check the character is exist or not
+    if (typeof map[character] === 'number') {
+      // character is repeated, update norepeat index
+      norepeat = index;
+    }
+
+    // check max length
+    var length = index - norepeat + 1;
+    if (length > max) {
+      // update max length
+      max = length;
+    }
+
+    // update the character's last index
+    map[character] = index;
+  }
+
+  return max;
 };
 
 // mocha testing
@@ -35,6 +61,34 @@ describe('Longest Substring Without Repeating Characters', function() {
     var output = lengthOfLongestSubstring(input);
 
     expect(output).to.equal(1);
+  });
+
+  it('abba', function() {
+    var input = 'abba';
+    var output = lengthOfLongestSubstring(input);
+
+    expect(output).to.equal(2);
+  });
+
+  it('a', function() {
+    var input = 'a';
+    var output = lengthOfLongestSubstring(input);
+
+    expect(output).to.equal(1);
+  });
+
+  it('abccbababc', function() {
+    var input = 'abccbababc';
+    var output = lengthOfLongestSubstring(input);
+
+    expect(output).to.equal(3);
+  });
+
+  it('', function() {
+    var input = '';
+    var output = lengthOfLongestSubstring(input);
+
+    expect(output).to.equal(0);
   });
 
 });
