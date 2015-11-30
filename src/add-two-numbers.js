04e20d8e66473a5a16a00311e947d97660af2a69
zlargon <zlargon@icloud.com>
2015-11-30 20:54:13 +0800

    solve add-two-numbers problem

--- src/add-two-numbers.js
+++ src/add-two-numbers.js
@@ -19,7 +19,38 @@ function ListNode(val) {
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
+  var root = null;
+  var prev = null;
 
+  var carry = 0;
+  while (l1 !== null || l2 !== null) {
+    var val = carry;
+    if (l1 !== null) val += l1.val;
+    if (l2 !== null) val += l2.val;
+
+    // carry
+    carry = Math.floor(val / 10);
+    val %= 10;
+
+    // add to linked list
+    if (root === null) {
+      root = new ListNode(val);
+      prev = root;
+    } else {
+      prev.next = new ListNode(val);
+      prev = prev.next;
+    }
+
+    // next node
+    if (l1 !== null) l1 = l1.next;
+    if (l2 !== null) l2 = l2.next;
+  }
+
+  if (carry > 0) {
+    prev.next = new ListNode(carry);
+  }
+
+  return root;
 };
 
 function linkedList(numbers) {
@@ -55,4 +86,43 @@ describe('Add Two Numbers', function() {
     expect(output).to.deep.equal(linkedList([7, 0, 8]));
   });
 
+  it('(9 -> 9 -> 9 -> 9) + (9 -> 9 -> 9)', function () {
+    var input = {
+      l1: linkedList([9, 9, 9, 9]),
+      l2: linkedList([9, 9, 9])
+    };
+    var output = addTwoNumbers(input.l1, input.l2);
+
+    expect(output).to.deep.equal(linkedList([8, 9, 9, 0, 1]));
+  });
+
+  it('(8 -> 8 -> 8) + (9 -> 8 -> 8 -> 8)', function () {
+    var input = {
+      l1: linkedList([8, 8, 8]),
+      l2: linkedList([9, 8, 8, 8])
+    };
+    var output = addTwoNumbers(input.l1, input.l2);
+
+    expect(output).to.deep.equal(linkedList([7, 7, 7, 9]));
+  });
+
+  it('(0) + (9 -> 8 -> 8 -> 8)', function () {
+    var input = {
+      l1: linkedList([0]),
+      l2: linkedList([9, 8, 8, 8])
+    };
+    var output = addTwoNumbers(input.l1, input.l2);
+
+    expect(output).to.deep.equal(linkedList([9, 8, 8, 8]));
+  });
+
+  it('(0) + (0)', function () {
+    var input = {
+      l1: linkedList([0]),
+      l2: linkedList([0])
+    };
+    var output = addTwoNumbers(input.l1, input.l2);
+
+    expect(output).to.deep.equal(linkedList([0]));
+  });
 });
